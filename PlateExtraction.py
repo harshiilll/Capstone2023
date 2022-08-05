85553d784417546e67d4d223c25c17ea8524b728
harshiilll <harshillpro@gmail.com>
2022-08-05 10:37:52 +0530

    minor changes

--- PlateExtraction.py
+++ PlateExtraction.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 def plot_images(img1, img2, title1="", title2=""):
     fig = plt.figure(figsize=[15,15])
-#     ax1 = fig.add_subplot(121)
-#     ax1.imshow(img1, cmap="gray")
-#     ax1.set(xticks=[], yticks=[], title=title1)
+    ax1 = fig.add_subplot(121)
+    ax1.imshow(img1, cmap="gray")
+    ax1.set(xticks=[], yticks=[], title=title1)
 
     ax2 = fig.add_subplot(122)
     ax2.imshow(img2, cmap="gray")
